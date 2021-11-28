df1c4a13351ab97af76888ec4352a8da5b6393ca
Janilson Coimbra <janilson.coimbra@gmail.com>
2021-11-27 21:27:11 -0300

    Versão inicial

---
+++ Aula 01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula_01
+{
+    class Program{
+        public static void Main(string[] args){
+            
+            int numero;
+            numero = Console.Read();
+            if(numero % 2 == 0) {
+                Console.WriteLine("PAR");
+            }else {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
