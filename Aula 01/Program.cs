a7217471ea1267ca3f23510af1d8383ee0a83939
Janilson Coimbra <janilson.coimbra@gmail.com>
2021-11-27 22:28:15 -0300

    atualizar

--- Aula 01/Program.cs
+++ Aula 01/Program.cs
@@ -3,15 +3,13 @@
 namespace Aula_01
 {
     class Program{
-        public static void Main(string[] args){
-            
-            int numero;
-            numero = Console.Read();
-            if(numero % 2 == 0) {
-                Console.WriteLine("PAR");
-            }else {
-                Console.WriteLine("IMPAR");
-            }
+        public static void Main(string[] args){ 
+           // Tipos de dados
+          string a = "JANILSON";
+          string b = "janilson";
+
+          Console.WriteLine(a == b);
+
         }
     }
 }
