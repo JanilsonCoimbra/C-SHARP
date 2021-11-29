1643378b258f6c3b0c913c8bebc0bacdcd5795de
Janilson Coimbra <janilson.coimbra@gmail.com>
2021-11-29 11:31:50 -0300

    Calculadora usando Suitch e Case

---
+++ Atividade 03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Atividade_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+            double v1, v2;
+            Console.WriteLine("Valor 01 :");
+            v1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 02 :");
+            v2 = Double.Parse(Console.ReadLine());
+            string opcao;
+            Console.WriteLine("Qual opcão ['-', '+', '/', '*', '%']:");
+            opcao = Console.ReadLine();
+            switch (opcao) {
+            case ("-"):
+            Console.WriteLine("----------------------------");
+            Console.WriteLine(v1 - v2);
+            break;
+            case ("+"):
+            Console.WriteLine("----------------------------");
+            Console.WriteLine(v1 + v2);
+            break;
+             case ("/"):
+             Console.WriteLine("----------------------------");
+             Console.WriteLine(v1 / v2);
+            break;
+             case ("*"):
+             Console.WriteLine("----------------------------");
+             Console.WriteLine(v1 * v2);
+                break;
+             case ("%"):
+             Console.WriteLine("----------------------------");
+             Console.WriteLine(v1 % v2);
+            break;
+            }
+            }
+
+        }
+    }
+}
