3af4d12fd30f4519eec7d85b73c4a97f98ae2bd6
Janilson Coimbra <janilson.coimbra@gmail.com>
2021-11-29 12:38:42 -0300

    Usando Forech para varrer arrays

---
+++ Atividade 05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] marcas = {"Volvo", "motorola", "sangsung"};
+
+            foreach (var item in marcas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
