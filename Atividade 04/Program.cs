faaeb42f38aaa5c5380725d1641e540f9f448d26
Janilson Coimbra <janilson.coimbra@gmail.com>
2021-11-29 12:27:03 -0300

    Usando FOR - Dizer o total de nuemros pares e impares e somar os totais

---
+++ Atividade 04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Atividade_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int par = 0;
+            int impar = 0;
+
+            for(int cont = 1; cont <= 20; cont++) {
+                if(cont % 2 == 0) {
+                    par++;
+                }else if(cont % 2 == 1) {
+                    impar++;
+                }
+            }
+            Console.WriteLine("Total de numeros pares :"+par);
+            Console.WriteLine("Total de numeros impares :"+impar);
+            Console.WriteLine("Total de numeros :"+(impar + par));
+        }
+    }
+}
